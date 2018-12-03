02baf54e8507f1cf04ca4ac427a9bfe1ba55edcf
Viola <vvvviola@yahoo.com>
2018-12-03 13:56:01 +0100

    modified esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        // windows linebreaks when not in production environment
-        "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
-    }
+    "extends": "airbnb"
 };
