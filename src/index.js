5ef46eb74e70f02d93e68c219646e4f2bba4aa33
Viola <vvvviola@yahoo.com>
2018-12-06 00:46:17 +0100

    reviewed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import express from 'express';
-import cors from 'cors';
-
 import router from './routes/incidentsRoute';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
-
-
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to iReporter');
 });
 
-
-app.use(cors());
 app.use('/api/v1', router);
 
 app.use(express.static('UI'));
